f55f80032af6794c37479b3b35e4dc96fbaeaa40
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-22 17:36:21 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Sotiris"
+  end
+
+  get '/hometown' do
+    "My hometown is Larissa "
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Bohemian Rhapsody by The Queen"
+  end
 end
